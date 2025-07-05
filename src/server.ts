9ece4db68ec0b0abcfa2aa4a399c2c26e78b4da7
Eliud Arceo <eliud1102@outlook.es>
2025-07-04 20:37:10 -0500

    cors

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,9 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://burger-express-six.vercel.app/"
+}));
 app.use(express.json());
 
 app.use("/api", contactRouter, userRouter);
